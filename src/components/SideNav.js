e61d9cbbd1655b710f1043170ebc8dc94816e836
SUMAN GHOSH <90588892+sumansingle@users.noreply.github.com>
2023-02-25 19:13:43 +0530

    Update SideNav.js

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -3,21 +3,30 @@ import { FaRegCalendarAlt, FaInbox, FaRegCalendar } from "react-icons/fa";
 import "../styles/App.css";
 
 const SideNav = (props) => {
-
-
-
   return (
     <ul className="side-nav">
-      <li onClick= {() => {props.change('INBOX')}}>
+      <li
+        onClick={() => {
+          props.change("INBOX");
+        }}
+      >
         <FaInbox />
         <div id="inbox-side">Inbox</div>
       </li>
-      <li onClick={() => {props.change('TODAY')}}>
+      <li
+        onClick={() => {
+          props.change("TODAY");
+        }}
+      >
         <FaRegCalendarAlt />
         <div id="today">Today</div>
       </li>
-      <li onClick={() => {props.change('NEXT')}}>
-        <FaRegCalendar/>
+      <li
+        onClick={() => {
+          props.change("NEXT");
+        }}
+      >
+        <FaRegCalendar />
         <div id="next">Next 7 Days</div>
       </li>
     </ul>
