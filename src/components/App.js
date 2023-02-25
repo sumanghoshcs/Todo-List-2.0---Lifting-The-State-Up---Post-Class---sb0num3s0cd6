e716b28385ba8e938d59e7c2a29ef7533a51442e
SUMAN GHOSH <90588892+sumansingle@users.noreply.github.com>
2023-02-25 19:10:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,25 @@
-import React, { useState } from 'react'
-import SideNav from './SideNav';
-import MainSection from './MainSection';
-import Header from './Header';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../styles/App.css';
+import React, { useState } from "react";
+import SideNav from "./SideNav";
+import MainSection from "./MainSection";
+import Header from "./Header";
+// import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../styles/App.css";
 const App = () => {
-
+  const [active, setActive] = useState("INBOX");
 
   return (
     <div id="main">
-    <Header />
-      <div className='row'>
-        <div className='col-md-2'>
-          <SideNav change={}/>
+      <Header />
+      <div className="row">
+        <div className="col-md-2">
+          <SideNav change={setActive} />
         </div>
-        <div className='col-md-10'>
-          <MainSection active = {}/>
+        <div className="col-md-10">
+          <MainSection active={active} />
         </div>
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
